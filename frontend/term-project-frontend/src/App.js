ba1f32702971d0281ffc4575e251b4406825371f
dhamid1 <125397550+dhamid1@users.noreply.github.com>
2023-11-11 21:21:18 -0500

    connect Wallet

--- frontend/term-project-frontend/src/App.js
+++ frontend/term-project-frontend/src/App.js
@@ -1,27 +1,40 @@
-
+// App.js
+import React, { useState } from 'react';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import connectToMetaMask from './Connection/connectWallet';
 
-const handleMetaMaskLink = () => {}
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+
+  const handleMetaMaskLink = async () => {
+    const connectionStatus = await connectToMetaMask();
+    if (connectionStatus) {
+      setIsConnected(true);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          
-        </p>
+        <p></p>
         <Row>
           <Col>ETHEREUM TRANSIT HUB</Col>
           <Col>
-            <Button variant="info" size='lg' onClick={handleMetaMaskLink} className="button-nowrap">Connect to MetaMask Wallet</Button>
+            {!isConnected && (
+              <Button variant="info" size="lg" onClick={handleMetaMaskLink} className="button-nowrap">
+                Connect to MetaMask Wallet
+              </Button>
+            )}
           </Col>
         </Row>
-        <Row><Col>Use Ethereum to buy tickets with airlines, MARC, Amtrak, and the Metro</Col></Row>
+        <Row>
+          <Col>Use Ethereum to buy tickets with airlines, MARC, Amtrak, and the Metro</Col>
+        </Row>
       </header>
-      
     </div>
   );
 }
