b8162e9e19a917132e83a157acbbdab285456a1c
Leonardo Conroy <leeleonard936@gmail.com>
2023-11-11 21:09:11 -0500

    metro input

--- frontend/term-project-frontend/src/pages/TransactionPage.js
+++ frontend/term-project-frontend/src/pages/TransactionPage.js
@@ -12,6 +12,22 @@ import Form from 'react-bootstrap/Form';
 
 
 function Trans() {
+    const [price, setPrice] = useState([]);
+    const [gasPrice, setGasPrice] = useState([]);
+    const [value, setValue] = useState([]);
+    const getPrice = () => {
+    axios.get('https://api.etherscan.io/api?module=stats&action=ethprice&apikey=2M6AYX86W1KF1V9KRMBHQEQ5U1BFGQ7D5S')
+    .then((res) => {setPrice(res.data.result.ethusd)})
+    axios.get('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=2M6AYX86W1KF1V9KRMBHQEQ5U1BFGQ7D5S')
+    .then((res) => {setGasPrice(res.data.result.ProposeGasPrice)})
+    
+  }
+  const handleChange = e => {
+    setValue(e.target.value)
+  }
+  useEffect(() => {
+    getPrice()
+  }, []);
     return(
     <div>
       <header>
@@ -33,9 +49,9 @@ function Trans() {
       </Tab>
       <Tab eventKey="metro" title="Metro">
         <Row>
+            <Col>
             Refill your Metro Card
-        </Row>
-        <Row>
+            </Col>
             <Col>
             <InputGroup className='text-nowrap'>
                 <InputGroup.Text id="dcmetro">$</InputGroup.Text>
@@ -45,8 +61,18 @@ function Trans() {
                 placeholder="0.00"
                 aria-label="refill your metro card"
                 aria-describedby="dcmetro"
+                onChange={handleChange}
           />
             </InputGroup>
+            </Col>
+            <Col>
+                {value/price} ETH
+            </Col>
+            <Col>
+                Gas Price: {gasPrice*21000}  
+                <a href='https://www.investopedia.com/terms/g/gwei-ethereum.asp#:~:text=Key%20Takeaways-,Gwei%20is%20a%20denomination%20of%20the%20cryptocurrency%20ether%20(ETH)%2C,to%20specify%20Ethereum%20gas%20prices.'>
+                    Gwei
+                </a>
             </Col>
         </Row>
         
@@ -54,7 +80,7 @@ function Trans() {
     </Tabs>
     </Row>
       </header>
-      </div>
+    </div>
     )
 }
 export default Trans;
